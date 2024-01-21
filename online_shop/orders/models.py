5a7ceaffe93cfd4f29b0bd08b63f8fa81090b62a
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-01-21 12:13:26 +0330

    Fixed the orders models and added products models

--- online_shop/orders/models.py
+++ online_shop/orders/models.py
@@ -1,20 +1,31 @@
 from django.db import models
 
-class Product(models.Model):
+class Order(models.Model):
+    customer = models.ForeignKey('customers.Customer', on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.ForeignKey('Discount', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"OrderItem #{self.id} - {self.product.name}"
+
+class Discount(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock_quantity = models.IntegerField()
+    percentage = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return self.name
 
-class Category(models.Model):
-    name = models.CharField(max_length=255)
+class FinalAmount(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    discounted_amount = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def __str__(self):
-        return self.name
-
-class ProductCategory(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
