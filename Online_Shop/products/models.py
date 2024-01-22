205a9491ef8c08b6c3675fbc2f0a67817101453e
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-01-22 22:11:14 +0330

    Added image field to product models

--- Online_Shop/products/models.py
+++ Online_Shop/products/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.db import models
+
+def upload_to(instance, filename):
+    # Always return the placeholder image URL if the image is not provided
+    return 'https://via.placeholder.com/300'
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.IntegerField()
+    image = models.ImageField(upload_to=upload_to, blank=True, null=True)
 
     def __str__(self):
         return self.name
