968c9faeab409ca569f445efc4027243ce23e4cb
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-01-22 14:13:01 +0330

    Added  models to admin panel

--- Online_Shop/orders/admin.py
+++ Online_Shop/orders/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Order, OrderItem, Discount, FinalAmount
+admin.site.register(Order)
+admin.site.register(OrderItem)
