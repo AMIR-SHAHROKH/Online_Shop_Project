562ac7ea17afb84bf42d192ed3a1b8a94d4916c1
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-01-23 19:03:11 +0330

    Fixed the views for the main page

--- Online_Shop/products/views.py
+++ Online_Shop/products/views.py
@@ -7,4 +7,12 @@ class ProductDetailView(View):
         products = Product.objects.all()
         return render(request, 'products/products_detail.html', {'product': products})
 
+class MainPageView(View):
+    template_name = 'products/main_page.html'
 
+    def get(self, request, *args, **kwargs):
+        # Add any additional context data you want to pass to the template
+        context = {
+            'some_data': 'This is some data for the template',
+        }
+        return render(request, self.template_name, context)
