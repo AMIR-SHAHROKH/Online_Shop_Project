b51761c29fd125972be0f730c67f68204ac88c3f
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-01-25 09:29:41 +0330

    Changed customers app name to users

--- Online_Shop/users/models.py
+++ Online_Shop/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Customer(models.Model):
+class Users(models.Model):
     username = models.CharField(max_length=255)
     email = models.EmailField()
     password = models.CharField(max_length=255)
