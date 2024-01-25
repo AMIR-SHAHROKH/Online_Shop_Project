979b191b864e8c8bfc97761f10f6db3f073896fb
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-01-26 00:00:26 +0330

    Added apps urls core url

--- Online_Shop/core/urls.py
+++ Online_Shop/core/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns =[
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
-            ]
+    path('', include('products.urls',namespace='Product related pages')),
+    path('profile' ,include('users.urls',namespace='user related pages')),
+    path('orders',include('orders.urls',namespace='order related pages'))
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
