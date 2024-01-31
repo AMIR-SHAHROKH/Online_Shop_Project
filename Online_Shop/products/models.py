077d4621b9f15fb289ecbedb24acc620a9389b69
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-02-01 00:28:51 +0330

    Modified the products models

--- Online_Shop/products/models.py
+++ Online_Shop/products/models.py
@@ -1,29 +1,27 @@
 from django.db import models
 from django.db import models
 from users.models import User
+from django.db import models
+from users.models import User
 
 def upload_to(instance, filename):
     # Always return the placeholder image URL if the image is not provided
     return 'https://via.placeholder.com/300'
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
     subcategory = models.CharField(max_length=255)
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='upload_to()',blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     description = models.TextField()
-    discount = models.DecimalField(max_digits=10, decimal_places=1)
-    price = models.IntegerField()
-    categories = models.ManyToManyField(Category, through='ProductCategory')
-
-class ProductCategory(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    discount = models.DecimalField(max_digits=10, decimal_places=1, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    categories = models.ManyToManyField(Category, related_name='products')
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField()
     comment = models.TextField()
-
-
