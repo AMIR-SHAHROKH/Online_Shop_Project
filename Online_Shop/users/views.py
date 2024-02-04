098b1cb625cb0e9d2786ef3aac4a324f9b21ac87
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-02-04 20:15:29 +0330

    Added login page class based views

--- Online_Shop/users/views.py
+++ Online_Shop/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'accounts/login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')  # Redirect to the home page after successful login
+        else:
+            # Return an error message or handle invalid login credentials
+            return render(request, 'accounts/login.html', {'error_message': 'Invalid username or password'})
+
+class LogoutView(View):
+    def get(self, request):
+        auth_logout(request)
+        return redirect('login')  # Redirect to the login page after logout
