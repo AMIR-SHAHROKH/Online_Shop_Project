ee6f04fc0dfa1124a5371d0f3cb2b2852ad8de78
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-02-04 20:57:54 +0330

    Added user login and sign up

--- Online_Shop/users/urls.py
+++ Online_Shop/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import signup
 
 urlpatterns = [
     # path('profile/', views.profile_view, name='profile'),
-    # Add more URL patterns as needed
+    path('signup/', signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]
