42be95e9faab7e34b8517d7e9f5b1cc583a4b6e2
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-02-07 04:19:45 +0330

    Fixed the users app urls

--- Online_Shop/users/urls.py
+++ Online_Shop/users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import signup
+
+app_name = "users"
 
 urlpatterns = [
-    # path('profile/', views.profile_view, name='profile'),
-    path('signup/', signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('', views.AccountView.as_view(), name='account'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('login-otp/', views.LoginWithEmailOTPView.as_view(), name='log in with otp'),
+    path('cart/', views.ShoppingCartView.as_view(), name='shopping_cart'),
 ]
