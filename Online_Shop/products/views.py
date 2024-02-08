5e43600c63af52a368258fb76e54fa3df5963ee2
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-02-08 09:27:59 +0330

    Added serializers to products

--- Online_Shop/products/views.py
+++ Online_Shop/products/views.py
@@ -1,6 +1,24 @@
 from django.views import View
 from django.shortcuts import render
 from .models import Product
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProductDetailView(View):
     def get(self, request, *args, **kwargs):
