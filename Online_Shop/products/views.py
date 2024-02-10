cdb2fb4ed7f2bf094d8bc2ee4c6a6bce798481f2
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-02-10 15:03:44 +0330

    Fixed the products and main page view

--- Online_Shop/products/views.py
+++ Online_Shop/products/views.py
@@ -1,10 +1,10 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
 from .models import Product
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
+from .models import Product , Category
 from .serializers import ProductSerializer
 
 class ProductList(APIView):
@@ -21,16 +21,19 @@ class ProductList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProductDetailView(View):
-    def get(self, request, *args, **kwargs):
-        products = Product.objects.all()
-        return render(request, 'products/products_detail.html', {'products': products})
+    def get(self, request, pk, slug, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk, slug=slug)
+        categories = Category.objects.all()
+        return render(request, 'products/products_detail.html', {'product': product, 'categories': categories})
+
   
 class MainPageView(View):
     template_name = 'products/main_page.html'
-
     def get(self, request, *args, **kwargs):
         # Add any additional context data you want to pass to the template
+        products = Product.objects.all()
+        categories = Category.objects.all()
         context = {
-            'some_data': 'This is some data for the template',
+            'products': products ,'categories': categories,
         }
-        return render(request, self.template_name, context)
+        return render(request, self.template_name,context)
