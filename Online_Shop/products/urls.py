5c0ba8ce887af58d22286e5d207814331f6fa52d
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-02-11 22:39:19 +0330

    Tried to fix sending otp emails

--- Online_Shop/products/urls.py
+++ Online_Shop/products/urls.py
@@ -6,6 +6,8 @@ from .views import MainPageView
 app_name = "products"
 
 urlpatterns = [
-    path('1', views.ProductDetailView.as_view() , name='product_detail'),
+    path('product/<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
     path('', views.MainPageView.as_view(), name='main_page'),
+    path('send-test-email/', views.send_test_email, name='send_test_email'),
 ]
+# 'product/<int:pk>/<slug:slug>/
--- Online_Shop/products/views.py
+++ Online_Shop/products/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Product , Category
 from .serializers import ProductSerializer
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
 class ProductList(APIView):
     def get(self, request, format=None):
@@ -21,8 +23,8 @@ class ProductList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProductDetailView(View):
-    def get(self, request, pk, slug, *args, **kwargs):
-        product = get_object_or_404(Product, pk=pk, slug=slug)
+    def get(self, request, slug, *args, **kwargs):
+        product = get_object_or_404(Product, slug=slug)
         categories = Category.objects.all()
         return render(request, 'products/products_detail.html', {'product': product, 'categories': categories})
 
@@ -37,3 +39,14 @@ class MainPageView(View):
             'products': products ,'categories': categories,
         }
         return render(request, self.template_name,context)
+
+
+def send_test_email(request):
+    send_mail(
+        'Test Email Subject',
+        'This is a test email message.',
+        'amir',
+        ['nafisehmehrabi90@gmail.com'],
+        fail_silently=False,
+    )
+    return HttpResponse('Test email sent successfully.')
