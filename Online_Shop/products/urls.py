3eb56e5b4c60ebb440197183c78990784ef52f6c
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-02-25 10:26:32 +0330

    Added api view rto urls and used that for the product details page

--- Online_Shop/products/urls.py
+++ Online_Shop/products/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import ProductDetailView
+from .views import *
 from .views import MainPageView
 
 app_name = "products"
 
 urlpatterns = [
-    path('product/<slug:slug>', views.ProductDetailView.as_view(), name='product_detail'),
+    path('api/product/<slug:slug>', views.ProductDetailAPIView.as_view(), name='product_detail_api'),
+    path('product/<slug:slug>', views.ProductDetailsView.as_view(), name='product_detail'),
     path('', views.MainPageView.as_view(), name='main_page'),
     path('send-test-email/', views.send_test_email, name='send_test_email'),
 ]
