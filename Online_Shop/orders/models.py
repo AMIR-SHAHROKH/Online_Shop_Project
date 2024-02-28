42a591b3282fb181b9bf78cfd3209c726cc6aeea
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-02-29 02:41:42 +0330

    Did slight changes in some fields in the orders model

--- Online_Shop/orders/models.py
+++ Online_Shop/orders/models.py
@@ -19,7 +19,7 @@ from django.db import models
 class Order(models.Model):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     order_date = models.DateTimeField(auto_now_add=True)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0, null=True)
     discount = models.ForeignKey('Discount', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
@@ -29,13 +29,27 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"OrderItem #{self.id} - {self.product.name}"
+    
+    def update_unit_price(self, *args, **kwargs):
+        # Retrieve the product associated with the order item
+        product = self.product
 
+        # Set the unit price of the order item to the price of the product
+        self.unit_price = product.price
+        
+        # Call the original save() method to save the order item
+        super().save(*args, **kwargs)
+    def calculate_total_price(self):
+        """Calculate the total price for this order item."""
+        return self.quantity * self.unit_price
+    
 class Discount(models.Model):
+
     name = models.CharField(max_length=255)
     percentage = models.DecimalField(max_digits=5, decimal_places=2)
     count = models.IntegerField()
