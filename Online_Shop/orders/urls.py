e6f8fab6668d7c67cc5e0be0759e5963df2210f5
AMIR-SHAHROKH <amirshahrokh82@yahoo.com>
2024-03-03 02:04:31 +0330

    added order detail items and add discount urls to the project

--- Online_Shop/orders/urls.py
+++ Online_Shop/orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import OrderListView, OrderDetailView, OrderListPageView, OrderDetailPageView, OrderItemCreatorAPIView,OrderItemsAPIView,OrderItemsView
+from .views import OrderListView, OrderDetailView, OrderListPageView, OrderDetailPageView, OrderItemCreatorAPIView,OrderItemsAPIView,OrderItemsView,ApplyDiscountView
 from django.contrib.auth.decorators import login_required
 
  
@@ -12,5 +12,6 @@ urlpatterns = [
     path('<int:order_id>/', OrderDetailPageView.as_view(), name='order_detail_page'),
     path('api/order-item-creator/', OrderItemCreatorAPIView.as_view(), name='order_item_creator_api'),
     path('api/order-items/<int:order_id>/', OrderItemsAPIView.as_view(), name='order-items'),
-    path('order-items/<int:order_id>/', OrderItemsView.as_view(), name='order-items')
+    path('order-items/<int:order_id>/', OrderItemsView.as_view(), name='order-items'),
+    path('apply-discount/<int:order_id>/', ApplyDiscountView.as_view(), name='apply_discount')
 ]
